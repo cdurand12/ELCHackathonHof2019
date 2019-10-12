b755588548a0698a657944672563f51731320c51
s0urc3d3v3l0pm3nt <melbing1@pride.hofstra.edu>
2019-10-12 04:19:24 -0400

    pass query to query(string[]) with each element separated in the list

--- sql_java/query.java
+++ sql_java/query.java
@@ -7,30 +7,48 @@ public class query {
     public static void main(String[] args) {
 
         try {
-            query(null);
+            String x[] = {"select", "*", "from", "products"};
+            query(x);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
+public static boolean setupSql(){
+    String[] runtimeArgs = {};
+    return true;
+}
 /*
 Pass your sql query as a String[] with each space represented as new element in the array
 For example to call 'describe products', pass query({"describe", "products");
  */
 public static void query(String[] q) throws Exception {
     //https://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
-    String[] runtimeArgs = new String[q.length + 5];
+    String[] runtimeArgs = new String[q.length + 5 + 1];
     runtimeArgs[0] = "C:\\tools\\mysql\\current\\bin\\mysql.exe";
     runtimeArgs[1] = "-u";
     runtimeArgs[2] = "root";
     runtimeArgs[3] = "-p1234";
     runtimeArgs[4] = "-e";
+    runtimeArgs[5] = "\"use";
+    runtimeArgs[6] = "elcdb;";
+
+    
+
+    for (int i = 0; i < q.length; i++) {
+        System.out.println(q[i]);
+        runtimeArgs[6 + i] = q[i];
+    }
+    runtimeArgs[runtimeArgs.length - 1] = "\"";
+
 
     Runtime runtime = Runtime.getRuntime();
     Process proc = runtime.exec(runtimeArgs);
 
-    BufferedReader output = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+    BufferedReader output = new BufferedReader(new InputStreamReader(proc.getInputStream()));
     BufferedReader err = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
 
     String temp = "";
     while ((temp = output.readLine()) != null){
